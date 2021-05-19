a0083a5a4a22dbf58426872aa228846707963019
unknown <alex.rocha@dtidigital.com.br>
2021-05-19 10:19:14 -0300

    finalizacao do clone do youtube

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
-import React from 'react'
-import { ThemeProvider, createMuiTheme, makeStyles } from '@material-ui/core'
+import React, { useState } from 'react';
+import { ThemeProvider, createMuiTheme, makeStyles } from '@material-ui/core';
+
 import Home from './home';
 
-
-
+const useStyles = makeStyles({
+  root: {},
+});
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
 
   const theme = createMuiTheme({
+    spacing: 4,
     palette: {
+      type: darkMode ? 'dark' : 'light',
       primary: {
-        main: '#f44336'
+        main: '#f44336',
       },
       secondary: {
-        main: '#3f51b5'
-      }
-    }
+        main: '#3EA6FF',
+      },
+      background: {
+        default: darkMode ? '#232323' : '#FFF',
+        dark: darkMode ? '#181818' : '#f4f6f8',
+        paper: darkMode ? '#232323' : '#FFF',
+      },
+    },
   });
+
+  const classes = useStyles();
 
   return (
     <ThemeProvider theme={theme}>
-      <Home/>
+      <Home darkMode={darkMode} setDarkMode={setDarkMode} />
     </ThemeProvider>
-  )
+  );
 }
 
 export default App;
